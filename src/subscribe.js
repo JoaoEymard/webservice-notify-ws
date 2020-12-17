ca559a87aaf78426cc1c0fe7d066f6fd345091dc
joaoeymard <eymarduarte12@hotmail.com>
2020-12-17 00:53:48 -0300

    Trazendo as libs para local

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,25 +1,19 @@
 const observers = {};
 
 module.exports = {
-
-    on: (event, command) => {
-        try {
-            
-            observers[event] = command;
-
-        } catch (error) {
-            console.error(error);
-        }
-    },
-
-    emit: (event, ...content) => {
-        try {
-            
-            observers[event](...content);
-
-        } catch (error) {
-            console.error(error);
-        }
+  on: (event, command) => {
+    try {
+      observers[event] = command;
+    } catch (error) {
+      console.error(error);
     }
+  },
 
-}
+  emit: (event, ...content) => {
+    try {
+      observers[event](...content);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
